42fbbf9ef674ea7e080107dcb82b200b73f0218a
Michael Muniz <michaelmuniz@michaels-mbp.lan>
2023-02-13 00:59:40 -0600

    added a series of option prompts for password generation in a function with IF statements.

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ var uppercaseLetters = lowercaseLetters.map(letter =>letter.toUpperCase());
 var generateBtn = document.querySelector("#generate");
 
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -27,8 +28,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// GeneratePassword based on user selection from the prompts
 function generatePassword(){
 
 
@@ -37,12 +44,34 @@ function generatePassword(){
 
 
 
-  // Test for button click
-  // console.log ("Button Clicked Test");
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Series of prompts for user to decide length of password that is a real number(not a letter or word) within a range of at least 8 but < 128 and select yes or no for other options (to include in password generation.
+function userSelects(){
+  optionsArray = [];
+  passwordLength = parseInt(prompt("Choose password length (8-128 characters)")); 
 
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+     alert("Password length has to be a number from 8 - 128 digits. Try again!");
+     return false;
+  }
 
+  if (confirm("Would you like numbers in your password?")) {
+    optionsArray = optionsArray.concat(numbers);
+  }
+
+  if (confirm("Would you like symbols in your password?")) {
+    optionsArray = optionsArray.concat(symbols);
+  }
+  if (confirm("Would you like lowercase letters in your password?")) {
+    optionsArray = optionsArray.concat(lowercaseLetters);
+  }
+  if (confirm("Would you like uppercase letters in your password?")) {
+    optionsArray = optionsArray.concat(uppercaseLetters);
+  }
+  return true;
+
+  
+}
