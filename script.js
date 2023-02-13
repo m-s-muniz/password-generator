1fbbdab139ae80887a8db236d7fb705dbe50ea0d
Michael Muniz <michaelmuniz@Michaels-MacBook-Pro.local>
2023-02-12 21:29:07 -0600

    added  variables and set up some arrays. Updatd Readme.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+// defined variables and set up some option arrays
+var passwordLength = 8;
+var optionsArray = [];
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+var symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*','+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', "`", "{", "|", "}", "~" ];
+
+var characterCodes = Array.from(Array(26)).map((_, i) => i +97);
+ // console.log(characterCodes); // Tested in console
+  
+var lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
+ // console.log(lowercaseLetters); //Tested in console
+
+var uppercaseLetters = lowercaseLetters.map(letter =>letter.toUpperCase());
+ // console.log(uppercaseLetters); //Tested in console
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -11,5 +29,20 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+
+
+
+
+
+
+
+  // Test for button click
+  // console.log ("Button Clicked Test");
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
